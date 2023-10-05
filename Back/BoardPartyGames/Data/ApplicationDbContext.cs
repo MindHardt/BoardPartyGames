6f21dbd9aa0f71d04052676df2916bf625126471
Игорь Бабин <igorbabin2002@gmail.com>
2023-10-05 13:14:17 +0500

    Added backend files

---
+++ Back/BoardPartyGames/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class ApplicationDbContext : IdentityDbContext<User>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        base.OnModelCreating(builder);
+    }
+}
---
+++ Back/BoardPartyGames/Data/Entities/User.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Data.Entities;
+
+public class User : IdentityUser
+{
+}
